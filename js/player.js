7e89ea72448ab79e7b7cd13e8161247db91b7387
AVerteC <alan.r.chen@gmail.com>
2025-06-02 14:58:48 -0700

    Got basic functionality of music player to work

--- js/player.js
+++ js/player.js
@@ -1,11 +1,12 @@
+
 class Jukebox {
     constructor(albums) {
         this.albums = []
     }
-    addAlbum = function(album) {
+    addAlbum = function (album) {
         this.albums.push(album)
     }
-    favoriteAlbum = function() {
+    favoriteAlbum = function () {
         let max = -1, fav
         for (let i = 0; i < this.albums.length; i++) {
             if (this.albums[i].played > max) {
@@ -23,10 +24,10 @@ class Album {
         this.title = title
         this.played = 0
     }
-    play = function() {
+    play = function () {
         this.played += 1
     }
-    display = function() {
+    display = function () {
         return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
     }
 }
@@ -49,3 +50,48 @@ album2.play()
 album3.play()
 
 console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+
+let currentlySelectedAlbum
+
+document.addEventListener("DOMContentLoaded", () => {
+    // console.log("Page loaded")
+
+    // Add all Albums to dropdown
+    for (let album of jbox.albums) {
+        // console.log(`${album.artist}, ${album.title}, ${album.played}`)
+
+        let newEntry = document.createElement("a")
+        newEntry.href = "#"
+        newEntry.textContent = `${album.artist} - ${album.title}`
+        newEntry.classList.add("dropdown-item")
+        console.log(`Just Added: ${album.artist} - ${album.title}`)
+        // Update current album selected
+        newEntry.addEventListener("click", function () {
+            currentlySelectedAlbum = album
+            let currSelectData = `Currently Selected: ${album.artist} - ${album.title}`
+            let currSelectedText = document.querySelector("#currSelectedAlbum")
+            currSelectedText.textContent = currSelectData
+        })
+        // console.log(newEntry)
+        document.querySelector("#dropdownMenu").appendChild(newEntry)
+    }
+
+
+    let dropdown = document.querySelector("#dropdownMenuButton")
+
+    dropdown.addEventListener("show.bs.dropdown", (e) => {
+
+    })
+
+    let playButton = document.querySelector("#playButton")
+    playButton.addEventListener("click", (e) => {
+        currentlySelectedAlbum.play()
+    })
+
+    let showFavAlbumBtn = document.querySelector("#showFavoriteAlbumButton")
+    showFavAlbumBtn.addEventListener("click", (e) => {
+        let favAlbumText = document.querySelector("#favoriteAlbum")
+        favAlbumText.textContent = jbox.favoriteAlbum()
+    })
+})
+
